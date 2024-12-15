88184f082d8fa2b7782116ae3cf870f40cecd114
brenwhat <brennanwong64@gmail.com>
2024-12-14 20:46:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -12,35 +12,45 @@ const categories = {
   4 : "Category 4" 
 }
 
-const artGallery = {
-    1: { title: 'wow im a cool title', author: 'Skibidi Toilet', grade: "6", category : 1, caption : "hey this is a demo caption just to see how itd look la tee ta tee ta tis i ajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkf", imageURL: "./workTypes/images/BingoPFP.jpg", coverImage: "./workTypes/images/BingoPFP.jpg"},
-    2: { title: 'very unique title', author: 'Name One & Name Two', grade: "7 & 8", category : 1, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/dababy.jpg", coverImage: "./workTypes/images/dababy.jpg"},
-    3: { title: 'nice art', author: 'I have an uncessarily long name', grade: "8", category : 2, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/fatdog.jpg", coverImage: "./workTypes/images/fatdog.jpg"},
-    4: { title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 3, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/egwin.jpg", coverImage: "./workTypes/images/squaresquirrel.jpg"},
-    5: { title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 4, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/hunterwallpaper2.png", coverImage: "./workTypes/images/RYANGOSLING.jpg"}
+// gallery 0 is for art, gallery 1 is for text
+
+const Gallery = {
+    1: { gallery: 0, title: 'wow im a cool title', author: 'Skibidi Toilet', grade: "6", category : 1, caption : "hey this is a demo caption just to see how itd look la tee ta tee ta tis i ajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkf", imageURL: "./workTypes/images/BingoPFP.jpg", coverImage: "./workTypes/images/BingoPFP.jpg"},
+    2: { gallery: 0, title: 'very unique title', author: 'Name One & Name Two', grade: "7 & 8", category : 1, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/dababy.jpg", coverImage: "./workTypes/images/dababy.jpg"},
+    3: { gallery: 0, title: 'nice art', author: 'I have an uncessarily long name', grade: "8", category : 2, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/550pixels.jpg", coverImage: "./workTypes/images/fatdog.jpg"},
+    4: { gallery: 0, title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 3, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/RYANGOSLING.jpg", coverImage: "./workTypes/images/squaresquirrel.jpg"},
+    5: { gallery: 0, title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 4, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/hunterwallpaper2.png", coverImage: "./workTypes/images/RYANGOSLING.jpg"},
+    6: { gallery: 1, title: 'Example Text 1', author: 'Brennan Wong', grade: "8", caption : "this is a short bit of text made to fill space and see how a text would look in the website.", imageURL: "./workTypes/text/exampletext1.pdf", coverImage: "./workTypes/images/dopecoverimage1.jpg"},
+    7: { gallery: 1, title: 'Example Text 2', author: 'Real Person', grade: "6", caption : "this is a short bit of text made to fill space and see how a text would look in the website. but this time i lengthened it for more pages", imageURL: "./workTypes/text/longerexampletext.pdf", coverImage: "./workTypes/images/dopecoverimage2.jpg"},
 };
-
-const textGallery = {
-    1: { title: 'Example Text 1', author: 'Brennan Wong', grade: "8", caption : "this is a short bit of text made to fill space and see how a text would look in the website.", imageURL: "./workTypes/text/exampletext1.pdf", coverImage: "./workTypes/images/dopecoverimage1.jpg"},
-    2: { title: 'Example Text 2', author: 'Real Person', grade: "6", caption : "this is a short bit of text made to fill space and see how a text would look in the website. but this time i lengthened it for more pages", imageURL: "./workTypes/text/longerexampletext.pdf", coverImage: "./workTypes/images/dopecoverimage2.jpg"},
-};
-
-const hybridGallery = {
-
-};
-
-const Galleries = [
-  artGallery,
-  textGallery,
-  hybridGallery,
-];
 
 function galleryRefresh() {
-  document.getElementById("gimage").src = localStorage.getItem("image") + '#toolbar=0';
-  document.getElementById("gtitle").innerHTML = localStorage.getItem("title");
-  document.getElementById("gauthor").innerHTML = localStorage.getItem("author") + ', Grade ' + localStorage.getItem("grade");
-  document.getElementById("gcategory").innerHTML = localStorage.getItem("category")
-  document.getElementById("gcaption").innerHTML = localStorage.getItem("caption");
+  var url = window.location.href;
+  var idStart = url.lastIndexOf('?');
+  var idLength = url.length - idStart;
+  console.log(idStart);
+
+  let id = url.slice((idLength * -1) + 1);
+  console.log(id);
+
+  document.getElementById("gimage").src = Gallery[id].imageURL  + '#toolbar=0';
+  document.getElementById("gtitle").innerHTML = Gallery[id].title;
+  document.getElementById("gauthor").innerHTML = Gallery[id].author + ', Grade ' + Gallery[id].grade;
+  document.getElementById("gcategory").innerHTML = categories[Gallery[id].category];
+  document.getElementById("gcaption").innerHTML = Gallery[id].caption;
+  
+  document.getElementById("expandimage").src = Gallery[id].imageURL  + '#toolbar=0';
+
+  if (Gallery[id].gallery == 1) {
+    let gimg = document.getElementById("gimage");
+    let eimg = document.getElementById("expandimage");
+
+    gimg.style.height = "100%";
+    gimg.style.width = "100%";
+
+    eimg.style.height = "100%";
+    eimg.style.width = "100%";
+  };
 }
 
 function setData(type1, grade1, cat1) {
@@ -83,14 +93,7 @@ function galleryDelete() {
 
 function loadPage(id) {
   console.log(type,id);
-  window.open('gallery.html');
-
-  localStorage.setItem("image", (Galleries[type])[id].imageURL);
-  localStorage.setItem("title", (Galleries[type])[id].title);
-  localStorage.setItem("author", (Galleries[type])[id].author);
-  localStorage.setItem("category", categories[(Galleries[type])[id].category])
-  localStorage.setItem("grade", (Galleries[type])[id].grade);
-  localStorage.setItem("caption", (Galleries[type])[id].caption);
+  window.location.href = ('gallery.html?' + id);
 };
 
 function openImage() {
@@ -98,14 +101,16 @@ function openImage() {
 };
 
 function fillArtCategory(id) {
-  const length = Object.keys(Galleries[type]).length;
+  const length = Object.keys(Gallery).length;
+  console.log(length);
   let counter = 0;
 
   for (let i=1; i<length+1; i++) {
-    let artGrade = (Galleries[type])[i].grade;
-    let artCat = (Galleries[type])[i].category;
+    let artGrade = Gallery[i].grade;
+    let artCat = Gallery[i].category;
+    let artGallery = Gallery[i].gallery;
 
-    if ((grade == 0 || artGrade.indexOf(grade) > -1) && (cat == 0 || artCat == cat)) {
+    if ((grade == 0 || artGrade.indexOf(grade) > -1) && (cat == 0 || artCat == cat) && artGallery == type) {
       counter += 1;
       createItem(id,i);
     }
@@ -142,22 +147,22 @@ function createItem(id,num) {
 
   var Art = document.createElement("img");
   Art.className = "Art"
-  Art.src = (Galleries[type])[num].coverImage;
+  Art.src = Gallery[num].coverImage;
 
   var captionback = document.createElement("div");
   captionback.className = "captionback";
 
   var title = document.createElement("p");
   title.className = "title";
-  title.innerHTML = (Galleries[type])[num].title;
+  title.innerHTML = Gallery[num].title;
 
   var category1 = document.createElement("p");
   category1.className = "category";
-  category1.innerHTML = categories[(Galleries[type])[num].category];
+  category1.innerHTML = categories[Gallery[num].category];
 
   var author = document.createElement("p");
   author.className = "author";
-  author.innerHTML = ((Galleries[type])[num].author + ", Grade " + (Galleries[type])[num].grade);
+  author.innerHTML = (Gallery[num].author + ", Grade " + Gallery[num].grade);
 
   
   artwhole.appendChild(Art);
