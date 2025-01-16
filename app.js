e12fc24df0d46ba9b780a2f8e9fe9c7e0e3ffa86
brenwhat <brennanwong64@gmail.com>
2025-01-15 20:35:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ const categories = {
 // gallery 0 is for art, gallery 1 is for text
 
 const Gallery = {
-    1: { gallery: 0, title: 'wow im a cool title', author: 'Skibidi Toilet', grade: "6", category : 1, caption : "hey this is a demo caption just to see how itd look la tee ta tee ta tis i ajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkf", imageURL: "./workTypes/images/BingoPFP.jpg", coverImage: "./workTypes/images/BingoPFP.jpg"},
-    2: { gallery: 0, title: 'very unique title', author: 'Name One & Name Two', grade: "7 & 8", category : 1, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/dababy.jpg", coverImage: "./workTypes/images/dababy.jpg"},
-    3: { gallery: 0, title: 'nice art', author: 'I have an uncessarily long name', grade: "8", category : 2, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/images/550pixels.jpg", coverImage: "./workTypes/images/fatdog.jpg"},
-    4: { gallery: 0, title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 3, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/RYANGOSLING.jpg", coverImage: "./workTypes/images/squaresquirrel.jpg"},
-    5: { gallery: 0, title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 4, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/images/hunterwallpaper2.png", coverImage: "./workTypes/images/RYANGOSLING.jpg"},
-    6: { gallery: 1, title: 'Example Text 1', author: 'Brennan Wong', grade: "8", caption : "this is a short bit of text made to fill space and see how a text would look in the website.", imageURL: "./workTypes/text/exampletext1.pdf", coverImage: "./workTypes/images/dopecoverimage1.jpg"},
-    7: { gallery: 1, title: 'Example Text 2', author: 'Real Person', grade: "6", caption : "this is a short bit of text made to fill space and see how a text would look in the website. but this time i lengthened it for more pages", imageURL: "./workTypes/text/longerexampletext.pdf", coverImage: "./workTypes/images/dopecoverimage2.jpg"},
+    1: { gallery: 0, title: 'wow im a cool title', author: 'Skibidi Toilet', grade: "6", category : 1, caption : "hey this is a demo caption just to see how itd look la tee ta tee ta tis i ajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkfajklsdfjlkasdljkf", imageURL: "./workTypes/BingoPFP.jpg", coverImage: "./workTypes/BingoPFP.jpg"},
+    2: { gallery: 0, title: 'very unique title', author: 'Name One & Name Two', grade: "7 & 8", category : 1, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/dababy.jpg", coverImage: "./workTypes/dababy.jpg"},
+    3: { gallery: 0, title: 'nice art', author: 'I have an uncessarily long name', grade: "8", category : 2, caption : "hey this is a demo caption just to see how itd look", imageURL: "./workTypes/550pixels.jpg", coverImage: "./workTypes/fatdog.jpg"},
+    4: { gallery: 0, title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 3, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/RYANGOSLING.jpg", coverImage: "./workTypes/squaresquirrel.jpg"},
+    5: { gallery: 0, title: 'egwin my beloved', author: 'Daghan', grade: "6", category : 4, caption : "this is my egwin hes very cool and this caption is also very cool. also hes a test for cover images being different", imageURL: "./workTypes/hunterwallpaper2.png", coverImage: "./workTypes/RYANGOSLING.jpg"},
+    6: { gallery: 1, title: 'Example Text 1', author: 'Brennan Wong', grade: "8", category : 3, caption : "this is a short bit of text made to fill space and see how a text would look in the website.", imageURL: "./workTypes/exampletext1.pdf", coverImage: "./workTypes/dopecoverimage1.jpg"},
+    7: { gallery: 1, title: 'Example Text 2', author: 'Real Person', grade: "6", category : 3, caption : "this is a short bit of text made to fill space and see how a text would look in the website. but this time i lengthened it for more pages", imageURL: "./workTypes/longerexampletext.pdf", coverImage: "./workTypes/dopecoverimage2.jpg"},
 };
 
 function galleryRefresh() {
@@ -54,7 +54,8 @@ function galleryRefresh() {
 }
 
 function setData(type1, grade1, cat1) {
-  let filtertext = document.getElementById("filters");
+  let categorytext = document.getElementById("allcategories");
+  let grtext = document.getElementById("allgrades");
 
   type = type1;
   if (grade1 != -1 && grade1 != 0) {
@@ -75,7 +76,8 @@ function setData(type1, grade1, cat1) {
     cattext = ("All Categories");
   };
 
-  filtertext.innerHTML = (gradetext + ", " + cattext)
+  categorytext.innerHTML = (cattext + '<i class="fa-solid fa-bars" style="margin-left: 0.7rem; margin-right: 0.7rem;"></i>')
+  grtext.innerHTML = (gradetext  + '<i class="fa-solid fa-bars" style="margin-left: 0.7rem; margin-right: 0.7rem;"></i>')
   galleryDelete();
 }
 
