f22a2c0ab92c744e5b4a9ce406f0c09bdbd42ebe
brenwhat <brennanwong64@gmail.com>
2025-03-06 15:32:28 -0800

    Update app.js

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ function setData(type1, grade1, cat1) {
   categorytext.innerHTML = (cattext + '<i class="fa-solid fa-bars" style="margin-left: 0.7rem; margin-right: 0.7rem;"></i>')
   grtext.innerHTML = (gradetext  + '<i class="fa-solid fa-bars" style="margin-left: 0.7rem; margin-right: 0.7rem;"></i>')
 
-  if (document.getElementById("missingtextdiv");) {
+  if (document.getElementById("missingtextdiv")) {
     document.getElementById("missingtextdiv").remove();
   }
   galleryDelete();
